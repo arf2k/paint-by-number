330589f512bae12d037c7b424d3e22bde852e279
https://learn.co/arf2k <foreman.alexander@gmail.com>
2023-02-16 09:57:31 -0500

    Image upload converts to grayscale

--- src/components/canvas/Canvas.js
+++ src/components/canvas/Canvas.js
@@ -1,3 +1,60 @@
+// import React, { useRef, useEffect } from "react";
+
+// const Canvas = ({ imageSrc, rows, cols, onSquareClick, selectedColor }) => {
+//   const canvasRef = useRef(null);
+
+//   useEffect(() => {
+//     if (!imageSrc || !rows || !cols) return;
+
+//     const canvas = canvasRef.current;
+//     const ctx = canvas.getContext("2d");
+//     const img = new Image();
+//     img.src = imageSrc;
+//     img.onload = () => {
+//       canvas.width = 500;
+//       canvas.height = canvas.width / (img.width / img.height);
+
+//       ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+
+//       const squareWidth = canvas.width / cols;
+//       const squareHeight = canvas.height / rows;
+
+//       for (let row = 0; row < rows; row++) {
+//         for (let col = 0; col < cols; col++) {
+//           ctx.strokeRect(
+//             col * squareWidth,
+//             row * squareHeight,
+//             squareWidth,
+//             squareHeight
+//           );
+//         }
+//       }
+//     };
+//   }, [imageSrc, rows, cols]);
+
+//   const handleCanvasClick = (event) => {
+//     const canvas = canvasRef.current;
+//     const ctx = canvas.getContext("2d");
+//     const x = event.clientX - canvas.offsetLeft;
+//     const y = event.clientY - canvas.offsetTop;
+//     const squareWidth = canvas.width / cols;
+//     const squareHeight = canvas.height / rows;
+//     const col = Math.floor(x / squareWidth);
+//     const row = Math.floor(y / squareHeight);
+//     const index = row * cols + col;
+
+//     console.log("Clicked square index:", index);
+//     onSquareClick(index);
+
+//     ctx.fillStyle = selectedColor
+//     ctx.fillRect(col * squareWidth, row * squareHeight, squareWidth, squareHeight);
+//   };
+
+//   return <canvas ref={canvasRef} onClick={handleCanvasClick} />;
+// };
+
+// export default Canvas;
+
 import React, { useRef, useEffect } from "react";
 
 const Canvas = ({ imageSrc, rows, cols, onSquareClick, selectedColor }) => {
@@ -15,6 +72,19 @@ const Canvas = ({ imageSrc, rows, cols, onSquareClick, selectedColor }) => {
       canvas.height = canvas.width / (img.width / img.height);
 
       ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+
+      const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+      const data = imageData.data;
+
+      // convert to grayscale
+      for (let i = 0; i < data.length; i += 4) {
+        const average = (data[i] + data[i + 1] + data[i + 2]) / 3;
+        data[i] = average;
+        data[i + 1] = average;
+        data[i + 2] = average;
+      }
+
+      ctx.putImageData(imageData, 0, 0);
 
       const squareWidth = canvas.width / cols;
       const squareHeight = canvas.height / rows;
@@ -46,7 +116,7 @@ const Canvas = ({ imageSrc, rows, cols, onSquareClick, selectedColor }) => {
     console.log("Clicked square index:", index);
     onSquareClick(index);
 
-    ctx.fillStyle = selectedColor
+    ctx.fillStyle = selectedColor;
     ctx.fillRect(col * squareWidth, row * squareHeight, squareWidth, squareHeight);
   };
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,60 @@
+// import React, { useState } from "react";
+// import Grid from "../components/grid/Grid";
+// import Palette from "../components/palette/Palette";
+// import GridChangeInput from "../components/grid-change-input/GridChangeInput";
+// import SquareGenerator from "../components/SquareGenerator";
+// import ImageUpload from "../components/image-upload/ImageUpload";
+// import Canvas from "../components/canvas/Canvas";
+
+// const Home = () => {
+//   const [imageSrc, setImageSrc] = useState("");
+//   const [gridSize, setGridSize] = useState({ rows: 0, cols: 0 });
+//   const [squares, setSquares] = useState([]);
+//   const [selectedColor, setSelectedColor] = useState("white");
+
+//   const changeColor = (index) => {
+//     const newSquares = squares.map((square, i) => {
+//       if (i === index) {
+//         return { ...square, color: selectedColor };
+//       }
+//       return square;
+//     });
+//     setSquares(newSquares);
+//   };
+
+//   const onColorClick = (color) => {
+//     console.log('Selected color:', color)
+//     setSelectedColor(color);
+//   };
+
+//   const onGridChange = (rows, cols) => {
+//     setGridSize({ rows, cols });
+//     setSquares(SquareGenerator(rows, cols));
+//   };
+
+//   const onSquareClick = (index) => {
+//     console.log("Clicked square index:", index);
+//     changeColor(index);
+//   };
+
+//   return (
+//     <div>
+//       <ImageUpload onImageUpload={setImageSrc} />
+//       <GridChangeInput gridSize={gridSize} onGridChange={onGridChange} />
+//       {gridSize.rows > 0 && gridSize.cols > 0 && (
+//         <>
+//           <Palette colors={["white", "red", "blue", "green"]} onColorClick={onColorClick} />
+//          {/* <Grid squares={squares} onSquareClick={onSquareClick} rows={gridSize.rows} cols={gridSize.cols} /> */}
+//         </>
+//       )}
+//       {imageSrc && gridSize.rows > 0 && gridSize.cols > 0 && (
+//         <Canvas imageSrc={imageSrc} rows={gridSize.rows} cols={gridSize.cols} onSquareClick={onSquareClick} selectedColor={selectedColor}/>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default Home;
 import React, { useState } from "react";
 import Grid from "../components/grid/Grid";
 import Palette from "../components/palette/Palette";
@@ -37,9 +94,14 @@ const Home = () => {
     changeColor(index);
   };
 
+  const onImageUpload = (imageSrc) => {
+    console.log('Image uploaded:', imageSrc);
+    setImageSrc(imageSrc);
+  };
+
   return (
     <div>
-      <ImageUpload onImageUpload={setImageSrc} />
+      <ImageUpload onImageUpload={onImageUpload} />
       <GridChangeInput gridSize={gridSize} onGridChange={onGridChange} />
       {gridSize.rows > 0 && gridSize.cols > 0 && (
         <>
